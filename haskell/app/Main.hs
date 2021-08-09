701ffe12a408a07603398c5cccf0d69f1380f590
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2021-08-10 01:15:24 +0900

    add using vbo and vao code

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
-import Control.Monad        (unless, void, when)
-import Control.Monad.Reader (ReaderT, runReaderT, ask, asks)
-import Control.Monad.State  (liftIO)
+import Control.Monad         (unless, void, when)
+import Control.Monad.Reader  (ReaderT, runReaderT, ask, asks)
+import Control.Monad.State   (liftIO)
+import Foreign.Marshal.Array (withArray)
+import Foreign.Ptr           (plusPtr, nullPtr, Ptr)
 
 import qualified Graphics.Rendering.OpenGL as GL
+import Graphics.Rendering.OpenGL (($=))
 import qualified Graphics.UI.GLFW          as GLFW
 
 import Lib
@@ -23,6 +26,9 @@ genVbo = GL.genObjectName >>= return
 
 genVao :: IO GL.VertexArrayObject
 genVao = GL.genObjectName >>= return
+
+offset :: Integral a => a -> Ptr b
+offset = plusPtr nullPtr . fromIntegral
 
 loopEnv :: Env a -> GLHandle -> IO a
 loopEnv f handle = do
@@ -47,14 +53,35 @@ main = do
         case m of 
             (Just window) -> do
                 GLFW.makeContextCurrent m 
+                --         | position |    |   color   |
+                let vertex = [  0,  1,      1, 0, 0, 1
+                             , -1, -1,      0, 1, 0, 1
+                             ,  1, -1,      0, 0, 1, 1] :: [GL.GLfloat]
+
                 vbo <- genVbo
+                GL.bindBuffer GL.ArrayBuffer $= Just vbo
+                withArray vertex $ \ptr -> do
+                    let bufferSize = fromIntegral $ 4 * (length vertex)
+                    GL.bufferData GL.ArrayBuffer $= (bufferSize, ptr, GL.StaticDraw)
+
                 vao <- genVao
+                GL.bindVertexArrayObject $= Just vao
+                let verLoc = GL.AttribLocation 0
+                    colLoc = GL.AttribLocation 1
+                GL.vertexAttribArray   verLoc $= GL.Enabled
+                GL.vertexAttribArray   colLoc $= GL.Enabled
+                GL.vertexAttribPointer verLoc $= (GL.ToFloat, GL.VertexArrayDescriptor 2 GL.Float (4 * 6) (offset 0))
+                GL.vertexAttribPointer colLoc $= (GL.ToFloat, GL.VertexArrayDescriptor 2 GL.Float (4 * 6) (offset 8))
+                GL.bindVertexArrayObject $= Nothing
+
                 program <- GL.createProgram
                 let handle = GLHandle { window  = window
                                       , vtx     = vbo
                                       , vao     = vao
                                       , program = program }
+
                 loopEnv loop handle
+
                 GL.deleteObjectName program
                 GL.deleteObjectName vao
                 GL.deleteObjectName vbo
