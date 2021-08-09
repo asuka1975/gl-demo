881a6660c362364a6cb86aa1a9de56cc6402158e
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2021-08-09 23:13:05 +0900

    create empty window in haskell

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,9 +1,62 @@
 module Main where
 
+import Control.Monad        (unless, void, when)
+import Control.Monad.Reader (ReaderT, runReaderT, ask, asks)
+import Control.Monad.State  (liftIO)
+
 import qualified Graphics.Rendering.OpenGL as GL
 import qualified Graphics.UI.GLFW          as GLFW
 
 import Lib
 
+data GLHandle = GLHandle
+    { window  :: !GLFW.Window
+    , vtx     :: !GL.BufferObject
+    , vao     :: !GL.VertexArrayObject
+    , program :: !GL.Program
+    }
+
+type Env = ReaderT GLHandle IO
+
+genVbo :: IO GL.BufferObject
+genVbo = GL.genObjectName >>= return
+
+genVao :: IO GL.VertexArrayObject
+genVao = GL.genObjectName >>= return
+
+loopEnv :: Env a -> GLHandle -> IO a
+loopEnv f handle = do
+    runReaderT f handle
+
+loop :: Env ()
+loop = do
+    window <- asks window
+
+    liftIO $ do
+        GLFW.swapBuffers window
+        GLFW.waitEvents
+
+    r <- liftIO $ GLFW.windowShouldClose window
+    unless r loop
+
 main :: IO ()
-main = someFunc
+main = do
+    r <- GLFW.init
+    when r $ do
+        m <- GLFW.createWindow 700 700 "triangle" Nothing Nothing
+        case m of 
+            (Just window) -> do
+                GLFW.makeContextCurrent m 
+                vbo <- genVbo
+                vao <- genVao
+                program <- GL.createProgram
+                let handle = GLHandle { window  = window
+                                      , vtx     = vbo
+                                      , vao     = vao
+                                      , program = program }
+                loopEnv loop handle
+                GL.deleteObjectName program
+                GL.deleteObjectName vao
+                GL.deleteObjectName vbo
+            Nothing -> return ()
+    GLFW.terminate
