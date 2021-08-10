27aea83ca0a74f61f54de22dd42f2bae437dc9a5
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2021-08-10 20:35:36 +0900

    initialize opengl in rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,8 @@
 extern crate gl;
 extern crate glfw;
 
-use glfw::{Context};
+use gl::types::*;
+use glfw::Context;
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
@@ -11,7 +12,17 @@ fn main() {
     glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
     window.make_current();
 
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    unsafe {
+        gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+    }
     while !window.should_close() {
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        
+
         window.swap_buffers();
         glfw.wait_events();
     }
