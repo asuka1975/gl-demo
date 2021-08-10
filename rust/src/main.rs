33257bf0f9c6a4b9e409abc72a9a497f3324e4d9
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2021-08-10 13:46:03 +0900

    create empty window in rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,18 @@
+extern crate gl;
 extern crate glfw;
 
 use glfw::{Context};
 
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    let (mut window, _) = glfw.create_window(700, 700, "triangle", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFWwindow");
+    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
+    window.make_current();
+
+    while !window.should_close() {
+        window.swap_buffers();
+        glfw.wait_events();
+    }
 }
