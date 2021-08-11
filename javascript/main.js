11c3d1ebdaae522f07a8e4e1d07dce2f8d7cb75e
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2021-08-11 22:52:52 +0900

    create gradation triangle in javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,81 @@
 vs = `#version 300 es
 
-layout (location = 0) in vec2 vertex;
-layout (location = 1) in vec4 color;
+in vec2 vertex;
+in vec4 color;
 
-layout (location = 0) out vec4 outColor;
+out vec4 vertexColor;
 
 void main() {
     gl_Position = vec4(vertex, 0, 1);
-    outColor = color;
+    vertexColor = color;
 }
 `;
 
 fs = `#version 300 es
+precision highp float;
 
-layout (location = 0) in vec4 color;
+in vec4 vertexColor;
 
-layout (location = 0) out vec4 outColor;
+out vec4 outColor;
 
 void main() {
-    outColor = color;
+    outColor = vertexColor;
 }
 `;
 
+function attachShader(gl, program, src, type) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, src);
+    gl.compileShader(shader);
+    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(gl.getShaderInfoLog(shader));
+    }
+    gl.attachShader(program, shader);
+    gl.deleteShader(shader);
+}
+
 function main() {
     const canvas = document.querySelector("#scene");
-    const gl = canvas.getContext("webgl");
+    const gl = canvas.getContext("webgl2");
 
     if(gl === null) {
         alert("failed initialize WebGL");
         return ;
     }
 
+    const vertex = [
+         0.0,  1.0,       1.0, 0.0, 0.0, 1.0,
+        -1.0, -1.0,       0.0, 1.0, 0.0, 1.0,
+         1.0, -1.0,       0.0, 0.0, 1.0, 1.0
+    ];
+
+    const program = gl.createProgram();
+    attachShader(gl, program, vs, gl.VERTEX_SHADER);
+    attachShader(gl, program, fs, gl.FRAGMENT_SHADER);
+    gl.linkProgram(program);
+
+    const vtx = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vtx);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertex), gl.STATIC_DRAW);
+
+    const vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+    const loc1 = gl.getAttribLocation(program, "vertex");
+    const loc2 = gl.getAttribLocation(program, "color");
+    gl.enableVertexAttribArray(loc1);
+    gl.enableVertexAttribArray(loc2);
+    gl.vertexAttribPointer(loc1, 2, gl.FLOAT, false, 24, 0);
+    gl.vertexAttribPointer(loc2, 4, gl.FLOAT, false, 24, 8);
+    gl.bindVertexArray(null);
+
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    
+    gl.useProgram(program);
+    gl.bindVertexArray(vao);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    gl.bindVertexArray(null);
+    gl.useProgram(null);
 }
 
 window.onload = main;
